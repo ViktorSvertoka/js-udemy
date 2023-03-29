5b8ea2dfc897d22749cb4fb9a89079a7a0d9fb93
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-03-30 01:41:13 +0300

    Feat(Files): added JS & HTML to lesson 19

---
+++ 19-ternarny-operator-task/19-script.js
@@ -0,0 +1,4 @@
+const age = prompt('Enter your age');
+const message = age >= 18 ? 'You can buy the alcohol.' : 'You can not buy the alcohol, you are too young.';
+console.log(message);
+alert(message);
