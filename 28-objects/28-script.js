0a61894a30f017e11e120653f693d7ab15fdf05a
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-04-03 00:14:52 +0300

    Feat(Files): added JS & HTML to lesson 28

---
+++ 28-objects/28-script.js
@@ -0,0 +1,18 @@
+myData = [
+   'YouRa',
+   'Allakhverdov',
+   1974,
+   'programming instructor',
+   ['Irina', 'Michael']
+];
+console.log(myData[4]);
+
+youraObject = {
+   name: 'YouRa',
+   lastname: 'Allakhverdov',
+   birthYear: 1975,
+   job: 'programming instructor',
+   familyMembers: ['Irina', 'Michael']
+};
+
+
