af5ce768331f8e12c2a043e9d74fcc2ad5f6f7aa
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-04-17 15:51:25 +0300

    Feat(Files): added JS & HTML to lesson 40

---
+++ 40-task-for-of/40-script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// We are creating a soccer betting app ⚽💰!
+// Let's say we are getting data about a specific game from a web service (the game variable below).
+// In this task, we will work with this data.
+
+const game = {
+  team1: 'REAL MADRID',
+  team2: 'BARCELONA',
+  players: [
+    [
+      'Courtois',
+      'Vazquez',
+      'Militao',
+      'Nacho',
+      'Mendy',
+      'Casemiro',
+      'Valverde',
+      'Modrich',
+      'Kroos',
+      'Vinicius',
+      'Benzema',
+    ],
+    [
+      'Stegen',
+      'Mingueza',
+      'Araujo',
+      'Lenglet',
+      'Dest',
+      'Busquets',
+      'Jong',
+      'Alba',
+      'Messi',
+      'Pedri',
+      'Dembele',
+    ],
+  ],
+  score: '2:1',
+  scored: ['Kroos', 'Benzema', 'Mingueza'],
+  date: 'Apr 10th, 2021',
+  odds: {
+    team1: 1.48,
+    draw: 2.53,
+    team2: 4.25,
+  },
+};
