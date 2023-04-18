ce2f539a4f88652b706db97a6d157da2352b5f6f
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-04-18 23:48:39 +0300

    Feat(Files): added JS & HTML to lesson 42

---
+++ 42-task-string/42-script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Write a program that takes a list of variable names written in underscore_case and converts them to camelCase.
+// Input will come from a textarea html element inserted into the DOM(see code below to insert elements)
+// and the transformation will happen when the button is clicked.
+
+// Test data (for inserting into the textarea including spaces):
+
+// underscore_case
+//   first_name
+// next_Variable
+//    Calculate_AGE
+// arrived_flight
+
+// This output should appear (5 separate console.log outputs):
+// underscoreCase
+// firstName
+// nextVariable
