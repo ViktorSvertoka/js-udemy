5f2ab57b10a7efc3912c555a55fbc80d62d558a2
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-04-19 00:22:53 +0300

    Feat(JS) task strings s done

--- 42-task-string/42-script.js
+++ 42-task-string/42-script.js
@@ -16,3 +16,28 @@
 // underscoreCase
 // firstName
 // nextVariable
+// calculateAge
+// arrivedFlight
+
+// Hints:
+// Remember which character defines a newline in a textbox
+// The solution should only work for a two-word variable like first_second
+// This is a tricky task, so start watching the solution in case you get stuck. Then pause and continue!
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const lines = text.split('\n');
+  // console.log(lines);
+  for (const line of lines) {
+    const [firstWord, secondWord] = line.toLowerCase().trim().split('_');
+    // console.log(firstWord, secondWord);
+    const output = `${firstWord}${secondWord.replace(
+      secondWord[0],
+      secondWord[0].toUpperCase(),
+    )}`;
+    console.log(output);
+  }
+});
